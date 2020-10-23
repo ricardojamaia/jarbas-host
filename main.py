18a8093f74b8bc3d08aa49f8a99a6dbbb86a1af8
Ricardo Maia <ricardo.ja.maia@gmail.com>
2020-10-23 17:24:23 +0000

    Clean up

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import cv2 as cv
 
+def main():
+    print(tf.__version__)
+    print(cv.__version__)
 
-print(tf.__version__)
-print(cv.__version__)
+
+if __name__ == '__main__':
+    main()
